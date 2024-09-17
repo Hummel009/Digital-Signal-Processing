1e59b0b814bcf4f0413bf0cbe0025fe7149e188d
Hummel009 <hummel.turbamentum@gmail.com>
2024-09-18 00:14:53 +0300

    Update ModAmplitude.kt

--- appLab1/src/main/kotlin/com/github/hummel/dsp/lab1/ModAmplitude.kt
+++ appLab1/src/main/kotlin/com/github/hummel/dsp/lab1/ModAmplitude.kt
@@ -1,8 +1,6 @@
 package com.github.hummel.dsp.lab1
 
 fun modulateAmplitude(signal: FloatArray, modulator: FloatArray): FloatArray {
-	require(signal.size == modulator.size) { "Arrays must be of the same length" }
-
 	return FloatArray(signal.size) { n ->
 		signal[n] * (1 + modulator[n])
 	}
