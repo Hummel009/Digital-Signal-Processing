e4525d5458b95cd45f44e1a3c234d441b8b18767
Hummel009 <hummel.turbamentum@gmail.com>
2024-09-22 17:48:04 +0300

    Update BlurMedian.kt

--- appLab3/src/main/kotlin/com/github/hummel/dsp/lab3/BlurMedian.kt
+++ appLab3/src/main/kotlin/com/github/hummel/dsp/lab3/BlurMedian.kt
@@ -12,7 +12,12 @@ fun applyMedianBlur(imageArray: Array<Array<IntArray>>, boxSize: Int): Array<Arr
 				val pixelValues = mutableListOf<Int>()
 				for (dy in -padSize..padSize) {
 					for (dx in -padSize..padSize) {
-						pixelValues.add(imageArray[y + dy][x + dx][c])
+						val newY = y + dy
+						val newX = x + dx
+
+						if (newY in 0 until height && newX in 0 until width) {
+							pixelValues.add(imageArray[newY][newX][c])
+						}
 					}
 				}
 				pixelValues.sort()
