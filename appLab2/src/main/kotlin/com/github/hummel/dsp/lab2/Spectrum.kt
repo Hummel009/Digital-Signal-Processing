426cd3db4ef18805d44164e5d0d76baab63a06c4
Hummel009 <hummel.turbamentum@gmail.com>
2024-09-27 13:54:03 +0300

    Update Spectrum.kt

--- appLab2/src/main/kotlin/com/github/hummel/dsp/lab2/Spectrum.kt
+++ appLab2/src/main/kotlin/com/github/hummel/dsp/lab2/Spectrum.kt
@@ -1,12 +1,9 @@
 package com.github.hummel.dsp.lab2
 
 fun decomposeSignal(fftResult: Array<Complex>): Pair<FloatArray, FloatArray> {
-	val amplitude = FloatArray(fftResult.size)
-	val phase = FloatArray(fftResult.size)
-
-	for (i in fftResult.indices) {
-		amplitude[i] = fftResult[i].magnitude
-		phase[i] = fftResult[i].phase
-	}
-	return Pair(amplitude, phase)
+	return fftResult.map {
+		it.magnitude
+	}.toFloatArray() to fftResult.map {
+		it.phase
+	}.toFloatArray()
 }
