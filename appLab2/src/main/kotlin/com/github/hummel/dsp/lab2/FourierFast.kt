de84c1fad18a5befa9abcc04bd6e78e037bd9b42
Hummel009 <hummel.turbamentum@gmail.com>
2024-10-16 17:02:13 +0300

    Update FourierFast.kt

--- appLab2/src/main/kotlin/com/github/hummel/dsp/lab2/FourierFast.kt
+++ appLab2/src/main/kotlin/com/github/hummel/dsp/lab2/FourierFast.kt
@@ -3,36 +3,36 @@ package com.github.hummel.dsp.lab2
 import kotlin.math.cos
 import kotlin.math.sin
 
-fun fft(signalValues: FloatArray): Array<Complex> = iteration(adaptLength(signalValues), false)
+fun fft(signalValues: FloatArray): Array<Complex> = fftRecursion(adaptLength(signalValues), false)
 
-fun ifft(spectrum: Array<Complex>): FloatArray = iteration(spectrum, true).map { it.real }.toFloatArray()
+fun ifft(spectrum: Array<Complex>): FloatArray = fftRecursion(spectrum, true).map { it.real }.toFloatArray()
 
-private fun iteration(data: Array<Complex>, invert: Boolean): Array<Complex> {
-	val N = data.size
-	if (N == 1) return data
+private fun fftRecursion(data: Array<Complex>, invert: Boolean): Array<Complex> {
+	val n = data.size
+	if (n == 1) return data
 
-	val result = Array(N) { Complex.ZERO }
-	val even = Array(N / 2) { Complex.ZERO }
-	val odd = Array(N / 2) { Complex.ZERO }
+	val result = Array(n) { Complex.ZERO }
+	val even = Array(n / 2) { Complex.ZERO }
+	val odd = Array(n / 2) { Complex.ZERO }
 
-	for (i in 0 until N step 2) {
+	for (i in 0 until n step 2) {
 		even[i / 2] = data[i]
 		odd[i / 2] = data[i + 1]
 	}
 
-	val evenRes = iteration(even, invert)
-	val oddRes = iteration(odd, invert)
+	val evenRes = fftRecursion(even, invert)
+	val oddRes = fftRecursion(odd, invert)
 
-	val ang = 2 * PI / N * if (invert) -1 else 1
+	val ang = 2 * PI / n * if (invert) -1 else 1
 	var w = Complex(1.0f, 0.0f)
 	val wn = Complex(cos(ang), sin(ang))
 
-	for (i in 0 until N / 2) {
+	for (i in 0 until n / 2) {
 		result[i] = evenRes[i] + w * oddRes[i]
-		result[i + N / 2] = evenRes[i] - w * oddRes[i]
+		result[i + n / 2] = evenRes[i] - w * oddRes[i]
 		if (invert) {
 			result[i] /= 2.0f
-			result[i + N / 2] /= 2.0f
+			result[i + n / 2] /= 2.0f
 		}
 		w *= wn
 	}
